9884b2de5b4b1353a57b2949d3bdbb9a6381a63b
aluno <aluno@embarquedigital.portodigital.local>
2024-02-05 14:59:50 -0300

    create-implement-challenge01

---
+++ Modulo1/D01/models/Pessoa.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace D01.models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public string DataDeNascimento { get; set; }
+        public float Altura { get; set; }
+    }
+}
