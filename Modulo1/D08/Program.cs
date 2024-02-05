27df215fda332bdf15c6fafe12802f20ec2fad1a
aluno <aluno@embarquedigital.portodigital.local>
2024-02-05 18:24:10 -0300

    create-implemet-challenge08

---
+++ Modulo1/D08/Program.cs
@@ -0,0 +1,13 @@
+﻿using D08.models;
+
+namespace D08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo triangulo = new Triangulo();
+            triangulo.CalcularArea();
+        }
+    }
+}
